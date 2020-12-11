ddc5fbf8dc32004a990415ec961b632ad2cd9780
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-12-12 00:07:23 +0530

    Added: AddressBook Backend Program with Output

---
+++ main/java/com/cg/addressbook/controller/ContactController.java
@@ -0,0 +1,46 @@
+package com.cg.addressbook.controller;
+
+import com.cg.addressbook.model.Contact;
+import com.cg.addressbook.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+
+public class ContactController {
+
+    @Autowired
+    ContactService contactService;
+
+    //http:localhost/address-book/ADDRESS_BOOK_1/contacts
+    @PostMapping("/{addressBookId}/contacts")
+    public Contact addContactToAddressBook(
+            @PathVariable String addressBookId, @Valid @RequestBody Contact newContact) {
+        return contactService.addContact(addressBookId, newContact);
+    }
+
+    //http:localhost/address-book/ADDRESS_BOOK_1/contacts/1
+    @DeleteMapping("/{addressBookId}/contacts/{contactId}")
+    public Contact removeContactFromAddressBook(@PathVariable String addressBookId,
+                                                @PathVariable String contactId) {
+        return contactService.removeContact(addressBookId, contactId);
+    }
+
+    //http:localhost/address-book/ADDRESS_BOOK_1/contacts
+    @GetMapping("/{addressBookId}/contacts")
+    public List<Contact> retrieveContactsFromAddressBook(@PathVariable String addressBookId) {
+        return contactService.retrieveContacts(addressBookId);
+    }
+
+    @GetMapping("/contacts")
+    public List<Contact> retrieveUniqueContactsFromAllAddressBooks(@RequestParam(value = "unique", defaultValue = "false") Boolean unique) {
+        return contactService.retrieveAllUniqueContacts(unique);
+    }
+}
+
---
+++ main/java/com/cg/addressbook/exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package com.cg.addressbook.exceptions;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String s) {
+        super(s);
+    }
+}
